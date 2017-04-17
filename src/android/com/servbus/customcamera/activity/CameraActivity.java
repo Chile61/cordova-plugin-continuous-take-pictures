eb6083b31260f1bc9f82483c5b4d2b902666fcdc
任跃兵 <cnryb@live.cn>
2017-04-17 15:09:29 +0800

    缩略图

--- src/android/com/servbus/customcamera/activity/CameraActivity.java
+++ src/android/com/servbus/customcamera/activity/CameraActivity.java
@@ -6,6 +6,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Rect;
 import android.hardware.Camera;
+import android.media.ThumbnailUtils;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.DisplayMetrics;
@@ -60,12 +61,13 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback, 
     private ImageView camera_close;
     private RelativeLayout homecamera_bottom_relative;
     private ImageView img_camera;
-
+    private ImageView img_picThumbnail;
 
     boolean mFocusEnd;
     int mDisplayRotate;
     int mViewWidth;
     int mViewHeight;
+    ArrayList<String> res = new ArrayList<String>();
 
     private TextView picCount;
 
@@ -100,6 +102,10 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback, 
 
         img_camera = (ImageView) findViewById(R.id.img_camera);
         img_camera.setOnClickListener(this);
+
+        img_picThumbnail = (ImageView) findViewById(R.id.img_picThumbnail);
+        img_picThumbnail.setAlpha(0.8f);
+
 
         //关闭相机界面按钮
         camera_close = (ImageView) findViewById(R.id.camera_close);
@@ -171,6 +177,7 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback, 
             //退出相机界面 释放资源
             case R.id.camera_close:
                 finish();
+                //returnData();
                 break;
 
             //闪光灯
@@ -260,6 +267,70 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback, 
         }
     }
 
+    /**
+     * Figure out what ratio we can load our image into memory at while still being bigger than
+     * our desired width and height
+     *
+     * @param srcWidth
+     * @param srcHeight
+     * @param dstWidth
+     * @param dstHeight
+     * @return
+     */
+    public static int calculateSampleSize(int srcWidth, int srcHeight, int dstWidth, int dstHeight) {
+        final float srcAspect = (float) srcWidth / (float) srcHeight;
+        final float dstAspect = (float) dstWidth / (float) dstHeight;
+
+        if (srcAspect > dstAspect) {
+            return srcWidth / dstWidth;
+        } else {
+            return srcHeight / dstHeight;
+        }
+    }
+
+    /**
+     * 根据指定的图像路径和大小来获取缩略图
+     * 此方法有两点好处：
+     * 1. 使用较小的内存空间，第一次获取的bitmap实际上为null，只是为了读取宽度和高度，
+     * 第二次读取的bitmap是根据比例压缩过的图像，第三次读取的bitmap是所要的缩略图。
+     * 2. 缩略图对于原图像来讲没有拉伸，这里使用了2.2版本的新工具ThumbnailUtils，使
+     * 用这个工具生成的图像不会被拉伸。
+     *
+     * @param imagePath 图像的路径
+     * @param width     指定输出图像的宽度
+     * @param height    指定输出图像的高度
+     * @return 生成的缩略图
+     */
+    private Bitmap getImageThumbnail(String imagePath, int width, int height) {
+        Bitmap bitmap = null;
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        // 获取这个图片的宽和高，注意此处的bitmap为null
+        bitmap = BitmapFactory.decodeFile(imagePath, options);
+        options.inJustDecodeBounds = false; // 设为 false
+        // 计算缩放比
+        int h = options.outHeight;
+        int w = options.outWidth;
+        int beWidth = w / width;
+        int beHeight = h / height;
+        int be = 1;
+        if (beWidth < beHeight) {
+            be = beWidth;
+        } else {
+            be = beHeight;
+        }
+        if (be <= 0) {
+            be = 1;
+        }
+        options.inSampleSize = be;
+        // 重新读入图片，读取缩放后的bitmap，注意这次要把options.inJustDecodeBounds 设为 false
+        bitmap = BitmapFactory.decodeFile(imagePath, options);
+        // 利用ThumbnailUtils来创建缩略图，这里要指定要缩放哪个Bitmap对象
+        bitmap = ThumbnailUtils.extractThumbnail(bitmap, width, height,
+                ThumbnailUtils.OPTIONS_RECYCLE_INPUT);
+        return bitmap;
+    }
+
 
     private void captrue() {
         mCamera.takePicture(null, null, new Camera.PictureCallback() {
@@ -277,11 +348,16 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback, 
                 String img_path_tmp = Environment.getExternalStorageDirectory() + "/0tmp/" + System.currentTimeMillis();
                 String img_path = img_path_tmp + ".jpg";
                 String img_path_t = img_path_tmp + "_t.jpg";
-
                 BitmapUtils.saveJPGE_After(context, saveBitmap, img_path, 100);
-                BitmapUtils.saveJPGE_After(context, saveBitmap, img_path_t, 20);
+
+                Bitmap bitmap_t = getImageThumbnail(img_path, 200, 200);
+                BitmapUtils.saveJPGE_After(context, bitmap_t, img_path_t, 100);
+
+                res.add(img_path);
 
                 returnData(img_path);
+                img_picThumbnail.setImageBitmap(bitmap_t);
+
                 picCount.setText(res.size() + "");
                 if (!bitmap.isRecycled()) {
                     bitmap.recycle();
