332d38f0b0b340e2e7c7dea43752cc2f53fddb81
任跃兵 <cnryb@live.cn>
2017-08-02 10:24:24 +0800

    暴露接口异常

--- www/ContinuousTakePictures.js
+++ www/ContinuousTakePictures.js
@@ -1,57 +1,54 @@
-cordova.define("cordova-plugin-continuous-take-pictures.ContinuousTakePictures", function (require, exports, module) {
 
-    var exec = cordova.require("cordova/exec");
+var exec = cordova.require("cordova/exec");
 
-    function ContinuousTakePictures() {
+function ContinuousTakePictures() {
 
 
+}
+
+/**
+ * Read code from scanner.
+ *
+ * @param {Function} successCallback 
+ * @param {Function} errorCallback
+ * @param config
+ */
+ContinuousTakePictures.prototype.takePictures = function (successCallback, errorCallback, config) {
+    config = config || {};
+
+    var args = [];
+    args[0] = config.dir || "";
+    args[1] = config.coverTpls || "";
+
+
+    if (errorCallback == null) {
+        errorCallback = function () {
+        };
     }
 
-    /**
-     * Read code from scanner.
-     *
-     * @param {Function} successCallback 
-     * @param {Function} errorCallback
-     * @param config
-     */
-    ContinuousTakePictures.prototype.takePictures = function (successCallback, errorCallback, config) {
-        config = config || {};
+    if (typeof errorCallback != "function") {
+        console.log("ContinuousTakePictures.scan failure: failure parameter not a function");
+        return;
+    }
 
-        var args = [];
-        args[0] = config.dir || "";
-        args[1] = config.coverTpls || "";
+    if (typeof successCallback != "function") {
+        console.log("ContinuousTakePictures.scan failure: success callback parameter must be a function");
+        return;
+    }
 
-
-        if (errorCallback == null) {
-            errorCallback = function () {
-            };
-        }
-
-        if (typeof errorCallback != "function") {
-            console.log("ContinuousTakePictures.scan failure: failure parameter not a function");
-            return;
-        }
-
-        if (typeof successCallback != "function") {
-            console.log("ContinuousTakePictures.scan failure: success callback parameter must be a function");
-            return;
-        }
-
-        exec(
-            function (result) {
-                successCallback(JSON.parse(result));
-            },
-            function (error) {
-                errorCallback(error);
-            },
-            'ContinuousTakePictures',
-            'ContinuousTakePictures',
-            args
-        );
-    };
+    exec(
+        function (result) {
+            successCallback(JSON.parse(result));
+        },
+        function (error) {
+            errorCallback(error);
+        },
+        'ContinuousTakePictures',
+        'ContinuousTakePictures',
+        args
+    );
+};
 
 
-    var ContinuousTakePictures = new ContinuousTakePictures();
-    module.exports = ContinuousTakePictures;
-
-});
+var ContinuousTakePictures = new ContinuousTakePictures();
+module.exports = ContinuousTakePictures;
